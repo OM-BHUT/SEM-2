1db19d133d2f330ddefb7c8b8cfa58aaf4eeb80c
OM-BHUT <codersarea12>
2024-04-15 15:22:36 +0530

    lab programs

---
+++ java lab/LAB 11/Q3.java
@@ -0,0 +1,11 @@
+// Write a program of writing binary file using multithreading.
+// Demonstrate use of join() and yield() interrupt()
+/**
+ * Q3
+ */
+public class Q3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
